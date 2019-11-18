950a32c7bc734e2ce20d645c2ae3616f82e5b725
ChristinaTu07 <57813426+ChristinaTu07@users.noreply.github.com>
2019-11-18 14:57:21 -0600

    Update team6.py

--- team6.py
+++ team6.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -6,9 +7,15 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'クリスティーナ 6' # Only 10 chars displayed.
+strategy_name = 'Mainly betray if neccessary'
+strategy_description = '''
+At the beginning, collude. Have a pattern of collude, betray, betray, and collude.
+'If their history has betray in list, betray all the way. If their history has collude in list, then betray all the way.
+
+'''
+
+
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
