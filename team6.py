23bcc9f2f892be263a8473af64fdea0693ef3002
ChristinaTu07 <57813426+ChristinaTu07@users.noreply.github.com>
2019-11-19 14:10:01 -0600

    Update team6.py

--- team6.py
+++ team6.py
@@ -8,9 +8,9 @@
 ####
 
 team_name = 'クリスティーナ 6' # Only 10 chars displayed.
-strategy_name = 'Mainly betray if neccessary'
+strategy_name = 'Mainly betraying'
 strategy_description = '''
-At the beginning, collude. Have a pattern of collude, betray, betray, and collude.
+At the beginning, collude.
 'If their history has betray in list, betray all the way. If their history has collude in list, then betray all the way.
 
 '''
@@ -18,12 +18,26 @@ At the beginning, collude. Have a pattern of collude, betray, betray, and collud
 
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+    if len(my_history) == 0:
+        return 'c'
+    if 'b' in their_history:
+        return 'b'
+    else:
+        return 'c'
+    if len(their_history[-1]) == 'b':
+        return 'b'
     
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
+    
+       
+
+
+        
+
+
+
+
+
+
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
@@ -60,7 +74,7 @@ if __name__ == '__main__':
               my_score=0,
               their_score=0,
               result='b'):
-         print 'Test passed'
+         print ('Test passed')
      # Test 2: Continue betraying if they collude despite being betrayed.
     test_move(my_history='bbb',
               their_history='ccc', 
