c20a2aff2fe48aa487341aeaad6bb5b33e166631
LuthKhairulhuda <57813451+LuthKhairulhuda@users.noreply.github.com>
2019-11-20 14:55:05 -0600

    Update team7.py

--- team7.py
+++ team7.py
@@ -7,15 +7,15 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = '∆v = ∫t1, [t0 |T(t)|/m(t)] * dt #7' # Only 10 chars displayed.
-strategy_name = 'Mimic if betrayed, Betray 5 Percent'
+team_name = 'Lucy K#7' # Only 10 chars displayed.
+strategy_name = 'betrayed if recent betray, Betray 1 Percent'
 strategy_description = '''The first 2 times, collude. 
 If their_score positive or higher than mine, betray first time. If they
 Haven't betrayed, then betray 15 percent of the time. Collude the rest. Check
-past 4 rounds, if betrayed, mimic for next 10 rounds. If alternates, always 
-return betray
+past 4 rounds, betray.
 '''
-    
+import random
+
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
@@ -32,21 +32,15 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    pround = their_history[-1]
-    rounds = 0
     
-    if len(their_history) <= 2:
+    if len(their_history) >= 1:
+        pround = their_history[-1]
+    if len(their_history) == 0:
         return 'c'
-    if 'b' in their_history[-4:]:
-        if their_history[-3] and their_history[-1] or their_history[-4] and their_history[-2] == 'b':
-            return 'b'
-        else:
-            rounds += 10
-            while rounds > 0:
-                rounds += -1
-                return pround
+    if 'b' in their_history[-2:]:
+        return 'b'
     else:
-        if random.random()<0.05:
+        if random.random()<0.01:
             return 'b'
         else:
             return 'c'
