c61652bf25f0a78f89e12e97ce26d42703e58eab
ChristinaTu07 <57813426+ChristinaTu07@users.noreply.github.com>
2019-11-21 13:40:23 -0600

    Update team6.py

--- team6.py
+++ team6.py
@@ -20,7 +20,7 @@ At the beginning, collude.
 def move(my_history, their_history, my_score, their_score):
     if len(my_history) == 0:
         return 'c'
-    if len(their_history[-1]) == 'b':
+    if len(their_history[-1]) == 'c':
         return 'b'
     else:
         return 'c'
